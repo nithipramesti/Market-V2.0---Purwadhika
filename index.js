26657b29048c1d19cb5056d1a8fcbfc25bd4a24e
nithipramesti <nithipramesti1508@gmail.com>
2021-06-22 17:15:24 +0800

    update function for input data

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-const inputData = () => {
-  const inputName = document.querySelector("#input--name").value;
-  const inputPrice = document.querySelector("#input--price").value;
-  const inputCategory = document.querySelector("#input--category").value;
-  const inputStock = document.querySelector("#input--stock").value;
+//PRODUCT LIST
+let products = [
+  { name: "Yellow shirt", price: 28, category: "Cloth", stock: 18 },
+  { name: "Mango", price: 6.5, category: "Fruit", stock: 15 },
+  { name: "Pink Sweater", price: 52, category: "Cloth", stock: 24 },
+  { name: "iPhone 11 64GB", price: 999, category: "Electronic", stock: 6 },
+  { name: "Sausage", price: 11, category: "Fast Food", stock: 12 },
+];
 
+//RENDERING LIST
+let fnRender = () => {
+  const productList = products.map((product) => {
+    let { name, price, category, stock } = product;
+    return `
+    <tr>
+      <td> ${category} </td>
+      <td> ${name} </td>
+      <td> $${price} </td>
+      <td> ${stock} </td>
+    </tr>
+    `;
+  });
+
+  document.querySelector("#table_data--body").innerHTML = productList.join("");
+};
+
+// INPUT DATA FUNCTION to TABLE DATA
+const fnInputData = () => {
+  // Defining variables
+  const name = document.querySelector("#input--name").value;
+  const price = document.querySelector("#input--price").value;
+  const category = document.querySelector("#input--category").value;
+  const stock = document.querySelector("#input--stock").value;
   const inputInputField = document.querySelectorAll(".input--input_field");
-
   const tableData = document.querySelector("#table-data");
 
-  tableData.innerHTML += `<tr>
-  <td>124345</td>
-  <td>${inputCategory}</td>
-  <td>${inputName}</td>
-  <td>${inputPrice}</td>
-  <td>${inputStock}</td>
-  </tr>`;
+  //Inserting to Products array
+  products.push({ name, price, category, stock });
 
+  //Update Table Data
+  fnRender();
+
+  // Clear input field after submitting
   inputInputField.forEach((input) => {
     input.value = "";
   });
+};
+
+fnRender();
+
+//FILTER DATA
+const fnFilterData = () => {
+  const filterName = document.querySelector("#filter--name");
+  console.log(filterName.value);
 };
